399e3dea98c646138f208656eaf0b91ee4f6d45b
Mariano <marianomgm93@gmail.com>
2026-04-16 19:35:48 -0300

    project/creation

---
+++ src/main/java/equipo7/AParkApp/AParkAppApplication.java
@@ -0,0 +1,13 @@
+package equipo7.AParkApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AParkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AParkAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/equipo7/AParkApp/AParkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package equipo7.AParkApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AParkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
