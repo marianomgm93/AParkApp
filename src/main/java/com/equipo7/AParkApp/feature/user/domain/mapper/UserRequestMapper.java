36a2939341289b15e388f89dc6c4c19f6d5a6d83
Mariano Gonzalez Moreno <marianomgm93@gmail.com>
2026-05-25 18:34:34 -0300

    Replace MapStruct mappers with Spring components
    Convert UserRequestMapper and UserResponseMapper from MapStruct @Mapper interfaces to Spring @Component classes implementing IMapper. Added org.springframework.stereotype.Component imports and changed the interface declarations to classes; mapping method signatures remain declared. Ensure concrete implementations or MapStruct generation is configured if needed.

--- src/main/java/com/equipo7/AParkApp/feature/user/domain/mapper/UserRequestMapper.java
+++ src/main/java/com/equipo7/AParkApp/feature/user/domain/mapper/UserRequestMapper.java
@@ -2,9 +2,10 @@ package com.equipo7.AParkApp.feature.user.domain.mapper;
 import com.equipo7.AParkApp.common.model.IMapper;
 import com.equipo7.AParkApp.feature.user.UserEntity;
 import com.equipo7.AParkApp.feature.user.domain.dto.UserRequest;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface UserRequestMapper extends IMapper<UserEntity, UserRequest>{
+@Component
+public class UserRequestMapper implements IMapper<UserEntity, UserRequest>{
     UserRequest toDTO(UserEntity userEntity);
     UserEntity toEntity(UserRequest userRequest);
 }
--- src/main/java/com/equipo7/AParkApp/feature/user/domain/mapper/UserResponseMapper.java
+++ src/main/java/com/equipo7/AParkApp/feature/user/domain/mapper/UserResponseMapper.java
@@ -2,9 +2,10 @@ package com.equipo7.AParkApp.feature.user.domain.mapper;
 
 import com.equipo7.AParkApp.feature.user.UserEntity;
 import com.equipo7.AParkApp.feature.user.domain.dto.UserResponse;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface UserResponseMapper extends IMapper<UserEntity, UserResponse> {
+@Component
+public class UserResponseMapper implements IMapper<UserEntity, UserResponse> {
     UserResponse toDTO(UserEntity userEntity);
     UserEntity toEntity(UserResponse userResponse);
 }
