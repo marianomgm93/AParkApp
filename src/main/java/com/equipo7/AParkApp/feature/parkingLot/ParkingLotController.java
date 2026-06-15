9f743c98f6d56c7a41eecd0ae5705be33c7d03ca
Tomas Cascales <tomascascales@hotmail.com>
2026-06-14 23:01:50 -0300

    UserManagment for revision

--- src/main/java/com/equipo7/AParkApp/feature/parkingLot/ParkingLotController.java
+++ src/main/java/com/equipo7/AParkApp/feature/parkingLot/ParkingLotController.java
@@ -2,11 +2,13 @@ package com.equipo7.AParkApp.feature.parkingLot;
 
 import com.equipo7.AParkApp.feature.parkingLot.Domain.DTO.ParkingLotRequest;
 import com.equipo7.AParkApp.feature.parkingLot.Domain.DTO.ParkingLotResponse;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,16 +22,17 @@ public class ParkingLotController {
     private final IParkingLotService parkingLotService;
 
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<ParkingLotResponse> create(@RequestBody ParkingLotRequest request) {
+    public ResponseEntity<ParkingLotResponse> create(@Valid @RequestBody ParkingLotRequest request) {
 
         ParkingLotResponse parkingLotResponse = parkingLotService.create(request);
 
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(parkingLotResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingLotResponse);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<ParkingLotResponse>> getAll() {
 
@@ -37,13 +40,14 @@ public class ParkingLotController {
         return ResponseEntity.ok(parkingLotService.getAllParkingLots());
     }
 
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/active")
     public ResponseEntity<List<ParkingLotResponse>> getAllActiveTrue() {
 
 
         return ResponseEntity.ok(parkingLotService.getAllActiveParkingLots());
     }
+    @PreAuthorize("hasRole('ADMIN')")
 
     @PutMapping("/{id}")
     public ResponseEntity<ParkingLotResponse> update(@PathVariable UUID id, @RequestBody ParkingLotRequest request) {
@@ -52,7 +56,7 @@ public class ParkingLotController {
         return ResponseEntity.ok(parkingLotService.update(id, request));
     }
 
-
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
 
@@ -61,6 +65,7 @@ public class ParkingLotController {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/{id}/restore")
     public ResponseEntity<Void> restore(@PathVariable UUID id) {
         parkingLotService.restore(id);
--- src/main/java/com/equipo7/AParkApp/feature/reservation/ReservationController.java
+++ src/main/java/com/equipo7/AParkApp/feature/reservation/ReservationController.java
@@ -6,7 +6,7 @@ import com.equipo7.AParkApp.feature.reservation.domain.dto.ReservationUpdateRequ
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,46 +18,58 @@ import java.util.UUID;
 public class ReservationController {
     private final ReservationService reservationService;
 
+    @PreAuthorize("hasAnyRole('OWNER', 'EMPLOYEE')")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ReservationResponseDTO> findAll() {
         return reservationService.getAll();
     }
 
+
+    @PreAuthorize("hasAnyRole('OWNER', 'EMPLOYEE')")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ReservationResponseDTO findById(@PathVariable UUID id) {
         return reservationService.getById(id);
     }
 
+
+    @PreAuthorize("hasAnyRole('OWNER', 'EMPLOYEE')")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ReservationResponseDTO create(@Valid @RequestBody ReservationRequestDTO request) {
         return reservationService.save(request);
     }
 
+    @PreAuthorize("hasAnyRole('OWNER', 'EMPLOYEE')")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ReservationResponseDTO modify(@PathVariable UUID id, @Valid @RequestBody ReservationUpdateRequest request) {
         return reservationService.update(id, request);
     }
 
+    @PreAuthorize("hasAnyRole('OWNER', 'EMPLOYEE')")
     @GetMapping("/vehicle")
     @ResponseStatus(HttpStatus.OK)
     public List<ReservationResponseDTO> findByPlate(@RequestParam String plate) {
         return reservationService.findByPlate(plate);
     }
 
+    @PreAuthorize("hasAnyRole('OWNER', 'EMPLOYEE')")
     @PatchMapping("/{id}/check-in")
     @ResponseStatus(HttpStatus.OK)
     public ReservationResponseDTO checkIn(@PathVariable UUID id) {
         return reservationService.checkIn(id);
     }
+
+    @PreAuthorize("hasAnyRole('OWNER', 'EMPLOYEE')")
     @PatchMapping("/{id}/check-out")
     @ResponseStatus(HttpStatus.OK)
     public ReservationResponseDTO checkOut(@PathVariable UUID id) {
         return reservationService.checkOut(id);
     }
+
+    @PreAuthorize("hasAnyRole('OWNER', 'EMPLOYEE')")
     @PatchMapping("/{id}/cancel")
     @ResponseStatus(HttpStatus.OK)
     public ReservationResponseDTO cancel(@PathVariable UUID id) {
