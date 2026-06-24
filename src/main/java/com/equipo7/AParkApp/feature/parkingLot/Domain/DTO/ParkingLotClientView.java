ef12ab48362ab55b6227b8b48c62d7b7047ee18c
Mariano Gonzalez Moreno <marianomgm93@gmail.com>
2026-06-24 13:12:52 -0300

    Fix parking lot view implementation

--- src/main/java/com/equipo7/AParkApp/feature/parkingLot/Domain/DTO/ParkingLotClientView.java
+++ src/main/java/com/equipo7/AParkApp/feature/parkingLot/Domain/DTO/ParkingLotClientView.java
@@ -2,11 +2,13 @@ package com.equipo7.AParkApp.feature.parkingLot.Domain.DTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ParkingLotClientView {
 
     private String name;
